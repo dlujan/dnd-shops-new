d82afeb788e7f12fa52217fa780ee856e370419b
Frank Lujan <franklujan@Franks-MBP.attlocal.net>
2025-04-25 18:04:48 -0500

    add icons to sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,25 +1,53 @@
 <template>
   <div class="sidebar">
-    <router-link class="router-link" to="/">General Store</router-link>
-    <router-link class="router-link" to="/blacksmith">Blacksmith</router-link>
-    <router-link class="router-link" to="/leatherworker"
-      >Leatherworker</router-link
+    <router-link class="router-link" to="/"
+      ><i className="ra ra-bell ra-2x"></i>
+      <span>General Store</span></router-link
     >
-    <router-link class="router-link" to="/fletcher">Fletcher</router-link>
-    <router-link class="router-link" to="/tailor">Tailor</router-link>
-    <router-link class="router-link" to="/tavern">Tavern</router-link>
-    <router-link class="router-link" to="/arcane">Arcane</router-link>
-    <router-link class="router-link" to="/potions">Potions</router-link>
-    <router-link class="router-link" to="/temple">Temple</router-link>
-    <router-link class="router-link" to="/jeweler">Jeweler</router-link>
+    <router-link class="router-link" to="/blacksmith"
+      ><i className="ra ra-large-hammer ra-2x"></i
+      ><span>Blacksmith</span></router-link
+    >
+    <router-link class="router-link" to="/leatherworker"
+      ><i className="ra ra-vest ra-2x"></i
+      ><span>Leatherworker</span></router-link
+    >
+    <router-link class="router-link" to="/fletcher"
+      ><i className="ra ra-target-arrows ra-2x"></i
+      ><span>Fletcher</span></router-link
+    >
+    <router-link class="router-link" to="/tailor"
+      ><i className="ra ra-slash-ring ra-2x"></i
+      ><span>Tailor</span></router-link
+    >
+    <router-link class="router-link" to="/tavern"
+      ><i className="ra ra-knife-fork ra-2x"></i
+      ><span>Tavern</span></router-link
+    >
+    <router-link class="router-link" to="/arcane"
+      ><i className="ra ra-crystal-ball ra-2x"></i
+      ><span>Arcane</span></router-link
+    >
+    <router-link class="router-link" to="/potions"
+      ><i className="ra ra-fizzing-flask ra-2x"></i
+      ><span>Potions</span></router-link
+    >
+    <router-link class="router-link" to="/temple"
+      ><i className="ra ra-sunbeams ra-2x"></i><span>Temple</span></router-link
+    >
+    <router-link class="router-link" to="/jeweler"
+      ><i className="ra ra-diamond ra-2x"></i><span>Jeweler</span></router-link
+    >
     <router-link class="router-link" to="/mounts-and-vehicles"
-      >Mounts and Vehicles</router-link
+      ><i className="ra ra-pawprint ra-2x"></i
+      ><span>Mounts and Vehicles</span></router-link
     >
   </div>
 </template>
 
 <script setup>
 import { useShopsStore } from "../stores/ShopsStore";
+import "rpg-awesome/css/rpg-awesome.min.css";
 const { shops } = useShopsStore();
 </script>
 
@@ -36,7 +64,9 @@ const { shops } = useShopsStore();
   flex-direction: column;
 }
 .router-link {
-  display: block;
+  display: flex;
+  align-items: center;
+  gap: 10px;
   color: #c9c9c9;
   text-decoration: none;
   padding: 10px;
